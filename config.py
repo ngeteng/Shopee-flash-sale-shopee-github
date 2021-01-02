8612862bfdf1b0c174211cac74746e338779a44f
Frederico BC <38299646+bangkitboss@users.noreply.github.com>
2021-01-02 16:13:22 +0700

    Add files via upload

---
+++ config.py
@@ -0,0 +1,4 @@
+
+pin_number ="969655"
+link_produk ="https://shopee.co.id/-COD-Micropack-Powerbank-10000-mAh-Fast-Charging-for-Oppo-VIVO-Samsung-Xiaomi-Redmi-iPhone-i.98362995.7245279462"
+cookiee = 'b0dEEsaI6h/BuT7Zfnad+M5H9fLdTNzV+PrCVm5s+UVZQfQoRrnXw7NS5SZXycgkzk3m8aqHRf+fuZEkUGnqmvCH7ak5AQFxHC4PIoIKqpwZ9S1k4HuPbTmZ89XcNnW9Sx5h+HUmilsVcBOCcVkR2ofoC5fzz0lk3mFcwYfW3sY='
